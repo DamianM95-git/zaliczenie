30dc49284890b0d7ff770381eff9d39a27657e7e
DamianM95-git <61930899+DamianM95-git@users.noreply.github.com>
2020-06-16 12:07:53 +0200

    Update Biblioteka.java
    Poprawny kod dla Biblioteka.java

--- Biblioteka.java
+++ Biblioteka.java
@@ -1,38 +1,53 @@
-import java.util.Scanner;
-import java.util.HashMap;
-import java.util.Map.Entry;
+import java.util.ArrayList;
+import java.util.List;
 
-class Main {
-  public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
-    String opcja;
-    do{
-    System.out.println("Podaj nazwę uzytkownika");
-    String user = scan.nextLine();
-    System.out.println("Podaj hasło dla danego użytkownika");
-    String passw = scan.nextLine();
+public class Biblioteka {
+  private int idGenerator = 1;
+  private List<Ksiazka> ksiazki;
+  private List<Uzytkownik> uzytkownicy;
 
-  System.out.println("Nazwa użytkownia to: \n" + user + "\na hasło: \n" +passw);
+  private List<WpisRejestru> rejestrWypozyczen;
 
-  HashMap<String,String> sampleMap = new HashMap<>();
-  sampleMap.put(user,passw);
-
-  System.out.println ("Podaj nazwę użytkownika, którą wprowadziłeś, a ja powiem jakie ma hasło");
-  String user1 = scan.nextLine();
-  for(String key : sampleMap.keySet()) 
-  {
-    String value = sampleMap.get(key);
-    System.out.println("Dla użytkownika: "+ key + " hasło to: " + value);
-      //else{
-      //System.out.println("Podałeś nieprawidlową nazwę użytkownika\n");
-      //continue;
+  public Biblioteka() {
+    this.ksiazki = new ArrayList<>();
+    this.uzytkownicy = new ArrayList<>();
+    this.rejestrWypozyczen = new ArrayList<>();
   }
-  
-  System.out.println("Jeśli chcesz zakończyć wybierz Q "+
-  "jesli nie, podaj inny znak. ");
-  opcja = scan.next();
+
+  public void dodajKsiazke(Ksiazka ksiazka){
+    ksiazka.setId(idGenerator);
+    idGenerator = idGenerator +1;
+    this.ksiazki.add(ksiazka);
+  }
+
+  public void dodajUzytkownika(Uzytkownik uzytkownik){
+    uzytkownik.setId(idGenerator++);
+    this.uzytkownicy.add(uzytkownik);
+  }
+
+  public boolean czyKsiazkaJestDostepna(Ksiazka ksiazka){
+    if (rejestrWypozyczen.isEmpty()){
+      return true;
     }
-    while ((!opcja.equalsIgnoreCase("Q")));
-    
-}
+    for (WpisRejestru wpis : rejestrWypozyczen){
+      if (wpis.getKsiazka().equals(ksiazka) && wpis.getDataZwrotu() == null){
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public void odzyskajZKopii(List<Ksiazka> ksiazki, List<Uzytkownik> uzytkownicy, List<WpisRejestru> rejestrWypozyczen){
+    this.ksiazki = ksiazki;
+    this.uzytkownicy = uzytkownicy;
+    this.rejestrWypozyczen = rejestrWypozyczen;
+  }
+
+  public List<Ksiazka> getKsiazki() {
+    return ksiazki;
+  }
+
+  public List<Uzytkownik> getUzytkownicy() {
+    return uzytkownicy;
+  }
 }
